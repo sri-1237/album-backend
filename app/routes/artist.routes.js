98551c0ad78911ebbb5eb066301f996bc8088a65
sadhanadondeti1 <sadhana.dondeti@eagles.oc.edu>
2022-06-04 03:25:57 -0500

    artist api routes added

--- app/routes/artist.routes.js
+++ app/routes/artist.routes.js
@@ -1,19 +1,18 @@
 module.exports = app => {
-    const artists = require("../controllers/artist.controller.js");
-    var router = require("express").Router();
-    // Create a new Lesson for a Tutorial
-    router.post("/:albumId/tracks/", tracks.create);
-    // Retrieve all Lessons for a Tutorial
-    router.get("/:albumId/tracks/", tracks.findAll);
-    // Retrieve all published Lessons for a Tutorial
-    router.get("/:albumId/tracks/published", tracks.findAllPublished);
-    // Retrieve a single Lesson with id
-    router.get("/:albumId/tracks/:id", tracks.findOne);
-    // Update a Lesson with id
-    router.put("/:albumId/tracks/:id", tracks.update);
-    // Delete a Lesson with id
-    router.delete("/:albumId/tracks/:id", tracks.delete);
-    // Delete all Lessons
-    router.delete("/:albumId/tracks/:id", tracks.deleteAll);
-    app.use('/api/albums', router);
-  };
+  const artists = require("../controllers/artist.controller.js");
+  const upload = require("../middleware/upload");
+  var router = require("express").Router();
+  // Create a new Tutorial
+  router.post("/",upload.single("file"), artists.create);
+  // Retrieve all Tutorials
+  router.get("/", artists.findAll);
+  // Retrieve a single Tutorial with id
+  router.get("/:id", artists.findOne);
+  // Update a Tutorial with id
+  router.put("/:id",upload.single("file"), artists.update);
+  // Delete a Tutorial with id
+  router.delete("/:id", artists.delete);
+  // Delete all Tutorials
+  router.delete("/", artists.deleteAll);
+  app.use('/api/artists+', router);
+};
